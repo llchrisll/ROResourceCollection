3d1c7eb43f5dca2f734f950ace6185b6709e4734
llchrisll <h0und3y3@gmail.com>
2022-01-06 00:40:03 +0100

    Readded twRO folder
    Signed-off-by: llchrisll <h0und3y3@gmail.com>

---
+++ twRO/System/CheckAttendance.lua
@@ -0,0 +1,63 @@
+-- Decompiled using luadec 2.2 rev: 895d923 for Lua 5.1 from https://github.com/viruscamp/luadec
+-- Command line: .\System\CheckAttendance.lub 
+
+-- params : ...
+-- function num : 0
+Config = {StartDate = 20211214, EndDate = 20220110}
+Reward = {
+{1, 25464, 1}
+, 
+{2, 101154, 2}
+, 
+{3, 22818, 1}
+, 
+{4, 23636, 3}
+, 
+{5, 6316, 1}
+, 
+{6, 25464, 2}
+, 
+{7, 23048, 5}
+, 
+{8, 23177, 3}
+, 
+{9, 14592, 1}
+, 
+{10, 101155, 1}
+, 
+{11, 25464, 3}
+, 
+{12, 12208, 1}
+, 
+{13, 12411, 2}
+, 
+{14, 6413, 1}
+, 
+{15, 12264, 1}
+, 
+{16, 25464, 4}
+, 
+{17, 12412, 3}
+, 
+{18, 101156, 2}
+, 
+{19, 14602, 2}
+, 
+{20, 12883, 4}
+}
+main = function()
+  -- function num : 0_0
+  result = InsertCheckAttendanceConfig(Config.EvendOnOff, Config.StartDate, Config.EndDate)
+  if not result then
+    return false, msg
+  end
+  for k,rewardtbl in pairs(Reward) do
+    result = InsertCheckAttendanceReward(rewardtbl[1], rewardtbl[2], rewardtbl[3])
+    if not result then
+      return false, msg
+    end
+  end
+  return true, "success"
+end
+
+
---
+++ twRO/System/RecommendedQuestInfoList_True.lua
@@ -0,0 +1,24 @@
+-- Decompiled using luadec 2.2 rev: 895d923 for Lua 5.1 from https://github.com/viruscamp/luadec
+-- Command line: .\System\RecommendedQuestInfoList_True.lub 
+
+-- params : ...
+-- function num : 0
+RecommendedQuestInfoList = {}
+RecommendedQuestInfoList = {
+{Title = "幻影海底洞", IconName = "ico_sc.bmp", Summary = "揭開無止境深淵海底洞的秘密!", BgName = "reco_94.bmp", 
+QuestInfo1 = {"在依斯魯得島發生了正在釣魚的希魯德失蹤的意外，", "自此3個月之後發現了希魯德的遺物，", "去<NAVI>[依斯魯得島]<INFO>izlude,132,49,0,101,0</INFO></NAVI>一起協尋失蹤者吧!"}
+, 
+QuestInfo2 = {"未知的大海", "深淵海底洞"}
+, 
+QuestInfo3 = {"^ff0000※該頁面為更新介紹而非任務說明，即使完成任務也不會刪除。^000000"}
+}
+; 
+[1] = {Title = "你好!歡迎來到RO仙境傳說的世界!", IconName = "ico_gq.bmp", Summary = "我們為新手玩家們加油喔!甘巴爹~", NpcSpr = "4_F_01", NpcNavi = "iz_ac01", NpcPosX = 102, NpcPosY = 44, BgName = "reco_01.bmp", 
+QuestInfo1 = {"歡迎歡迎!", "克里圖拉學院為了協助新手冒險家，提供各種有益的資訊。如果有尚未造訪克里圖拉學院的新手冒險家們，記得過來領取有益的資訊和禮包喔!"}
+, 
+QuestInfo2 = {"$$初學者$$歡迎!!", "新手%%禮包%%棒棒@!!讚讚@!!", "&*轉職*&諮詢 OK!!"}
+, 
+QuestInfo3 = {"^ff0000※本頁面不是任務，而是更新介紹，即使完成任務也不會刪除。^000000"}
+}
+}
+
